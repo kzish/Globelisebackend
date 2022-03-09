3e5355e50eb00b794c33460d491cac821a9c90df
Mike <mike@globelise.com>
2022-03-09 12:49:16 -0800

    Make errors consistent

--- eor-admin-microservice/src/error.rs
+++ eor-admin-microservice/src/error.rs
@@ -20,18 +20,20 @@ impl IntoResponse for Error {
     fn into_response(self) -> Response {
         let (status, message) = match self {
             Error::UnavailableEmail => (StatusCode::UNPROCESSABLE_ENTITY, "Email is unavailable"),
-            Error::BadRequest(message) => (StatusCode::BAD_REQUEST, "Bad Request"),
-            Error::Unauthorized(message) => {
-                return StatusCode::UNAUTHORIZED.into_response();
-            }
-            Error::Internal(message) => {
-                return StatusCode::INTERNAL_SERVER_ERROR.into_response();
-            }
             Error::UnsupportedImageFormat => (
                 StatusCode::UNSUPPORTED_MEDIA_TYPE,
                 "Image must be PNG or JPEG",
             ),
+            Error::BadRequest(message) => (StatusCode::BAD_REQUEST, message),
+            Error::Unauthorized(message) => {
+                eprintln!("{message}");
+                return StatusCode::UNAUTHORIZED.into_response();
+            }
             Error::PayloadTooLarge(message) => (StatusCode::PAYLOAD_TOO_LARGE, message),
+            Error::Internal(message) => {
+                eprintln!("{message}");
+                return StatusCode::INTERNAL_SERVER_ERROR.into_response();
+            }
         };
         (status, message).into_response()
     }
