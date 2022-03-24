80d6b856dd6d1c8a719f39c440100d36013408c0
Mike <mike@globelise.com>
2022-03-24 09:42:44 -0700

    Move database code into submodules

---
+++ contractor-management-microservice/src/invoice/database.rs
@@ -0,0 +1,77 @@
+use common_utils::error::GlobeliseResult;
+
+use crate::{common::ulid_to_sql_uuid, database::Database};
+
+use super::{
+    InvoiceGroupIndex, InvoiceGroupIndexQuery, InvoiceIndividualIndex, InvoiceIndividualIndexQuery,
+};
+
+impl Database {
+    /// Index individual invoice
+    pub async fn invoice_individual_index(
+        &self,
+        query: InvoiceIndividualIndexQuery,
+    ) -> GlobeliseResult<Vec<InvoiceIndividualIndex>> {
+        let index = sqlx::query(
+            "
+                SELECT
+                    ulid, invoice_group_ulid, contract_ulid, invoice_id,
+                    invoice_due, invoice_status, invoice_amount
+                FROM
+                    invoice_individual_index
+                WHERE
+                    invoice_group_ulid = $1 AND
+                    ($2 IS NULL OR (invoice_name ~* $2)) AND
+                    ($3 IS NULL OR (contractor_ulid ~* $3)) AND
+                    ($4 IS NULL OR (client_ulid ~* $4)) AND
+                LIMIT $3 OFFSET $4",
+        )
+        .bind(ulid_to_sql_uuid(query.invoice_group_ulid))
+        .bind(query.search_text)
+        .bind(query.contractor_ulid.map(ulid_to_sql_uuid))
+        .bind(query.client_ulid.map(ulid_to_sql_uuid))
+        .bind(query.per_page)
+        .bind((query.page - 1) * query.per_page)
+        .fetch_all(&self.0)
+        .await?
+        .into_iter()
+        .map(InvoiceIndividualIndex::from_pg_row)
+        .collect::<GlobeliseResult<Vec<InvoiceIndividualIndex>>>()?;
+
+        Ok(index)
+    }
+
+    /// Index group invoice
+    pub async fn invoice_group_index(
+        &self,
+        query: InvoiceGroupIndexQuery,
+    ) -> GlobeliseResult<Vec<InvoiceGroupIndex>> {
+        let index = sqlx::query(
+            "
+                SELECT
+                    ulid, invoice_group_ulid, contract_ulid, invoice_id,
+                    invoice_due, invoice_status, invoice_amount
+                FROM
+                    invoice_group_index
+                WHERE
+                    invoice_group_ulid = $1 AND
+                    ($2 IS NULL OR (invoice_name ~* $2)) AND
+                    ($3 IS NULL OR (contractor_ulid ~* $3)) AND
+                    ($4 IS NULL OR (client_ulid ~* $4)) AND
+                LIMIT $3 OFFSET $4",
+        )
+        .bind(ulid_to_sql_uuid(query.invoice_group_ulid))
+        .bind(query.search_text)
+        .bind(query.contractor_ulid.map(ulid_to_sql_uuid))
+        .bind(query.client_ulid.map(ulid_to_sql_uuid))
+        .bind(query.per_page)
+        .bind((query.page - 1) * query.per_page)
+        .fetch_all(&self.0)
+        .await?
+        .into_iter()
+        .map(InvoiceGroupIndex::from_pg_row)
+        .collect::<GlobeliseResult<Vec<InvoiceGroupIndex>>>()?;
+
+        Ok(index)
+    }
+}
--- contractor-management-microservice/src/tax_report/mod.rs
+++ contractor-management-microservice/src/tax_report/mod.rs
@@ -14,67 +14,9 @@ use serde_with::{base64::Base64, serde_as, TryFromInto};
 use sqlx::{postgres::PgRow, FromRow, Row};
 use user_management_microservice_sdk::{AccessToken as UserAccessToken, Role};
 
-use crate::{
-    common::{ulid_from_sql_uuid, ulid_to_sql_uuid},
-    database::{Database, SharedDatabase},
-};
+use crate::{common::ulid_from_sql_uuid, database::SharedDatabase};
 
-impl Database {
-    /// Indexes tax report.
-    pub async fn tax_report_index(
-        &self,
-        query: TaxReportIndexQuery,
-    ) -> GlobeliseResult<Vec<TaxReportIndex>> {
-        let index = sqlx::query_as(
-            "
-            SELECT
-                ulid, client_name, contractor_name, contract_name, tax_interval,
-                tax_name, begin_period, end_period, country
-            FROM
-                tax_reports_index
-            WHERE
-                ($1 IS NULL OR client_ulid = $1) AND
-                ($2 IS NULL OR contractor_ulid = $2) AND
-                ($3 IS NULL OR (client_name ~* $3 OR contractor_name ~* $3))
-            LIMIT $4 OFFSET $5",
-        )
-        .bind(query.client_ulid.map(ulid_to_sql_uuid))
-        .bind(query.contractor_ulid.map(ulid_to_sql_uuid))
-        .bind(query.search_text)
-        .bind(query.per_page)
-        .bind((query.page - 1) * query.per_page)
-        .fetch_all(&self.0)
-        .await?;
-
-        Ok(index)
-    }
-
-    /// Create tax report
-    pub async fn create_tax_report(&self, query: CreateTaxReportIndex) -> GlobeliseResult<()> {
-        sqlx::query(
-            "
-            INSERT INTO tax_reports
-            (ulid, client_ulid, contractor_ulid, contract_ulid, tax_interval,
-            tax_name, begin_period, end_period, country, tax_report_file)
-            VALUES
-            ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)",
-        )
-        .bind(ulid_to_sql_uuid(Ulid::generate()))
-        .bind(ulid_to_sql_uuid(query.client_ulid))
-        .bind(ulid_to_sql_uuid(query.contractor_ulid))
-        .bind(query.contract_ulid.map(ulid_to_sql_uuid))
-        .bind(query.tax_interval)
-        .bind(query.tax_name)
-        .bind(query.begin_period)
-        .bind(query.end_period)
-        .bind(query.country)
-        .bind(query.tax_report_file)
-        .execute(&self.0)
-        .await?;
-
-        Ok(())
-    }
-}
+mod database;
 
 /// List the tax reports
 pub async fn user_tax_report_index(
