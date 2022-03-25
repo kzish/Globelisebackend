7f2914b8abbf79f452ed1b4b00388959f059c03a
Mike <mike@globelise.com>
2022-03-25 13:23:56 -0700

    Add endpoints for payslips

---
+++ contractor-management-microservice/src/payslips/database.rs
@@ -0,0 +1,63 @@
+use common_utils::error::GlobeliseResult;
+
+use rusty_ulid::Ulid;
+
+use crate::{common::ulid_to_sql_uuid, database::Database};
+
+use super::{CreatePayslipsIndex, PayslipsIndex, PayslipsIndexQuery};
+
+impl Database {
+    /// Indexes tax report.
+    pub async fn payslips_index(
+        &self,
+        query: PayslipsIndexQuery,
+    ) -> GlobeliseResult<Vec<PayslipsIndex>> {
+        let index = sqlx::query_as(
+            "
+            SELECT
+                ulid, client_name, contractor_name, contract_name, payslip_title,
+                payment_date, begin_period, end_period
+            FROM
+                payslips_index
+            WHERE
+                ($1 IS NULL OR client_ulid = $1) AND
+                ($2 IS NULL OR contractor_ulid = $2) AND
+                ($3 IS NULL OR (client_name ~* $3 OR contractor_name ~* $3))
+            LIMIT $4 OFFSET $5",
+        )
+        .bind(query.client_ulid.map(ulid_to_sql_uuid))
+        .bind(query.contractor_ulid.map(ulid_to_sql_uuid))
+        .bind(query.search_text)
+        .bind(query.per_page)
+        .bind((query.page - 1) * query.per_page)
+        .fetch_all(&self.0)
+        .await?;
+
+        Ok(index)
+    }
+
+    /// Create tax report
+    pub async fn create_payslip(&self, query: CreatePayslipsIndex) -> GlobeliseResult<()> {
+        sqlx::query(
+            "
+            INSERT INTO payslips
+            (ulid, client_ulid, contractor_ulid, contract_ulid, payslip_title,
+            payment_date, begin_period, end_period, payslip_file)
+            VALUES
+            ($1, $2, $3, $4, $5, $6, $7, $8, $9)",
+        )
+        .bind(ulid_to_sql_uuid(Ulid::generate()))
+        .bind(ulid_to_sql_uuid(query.client_ulid))
+        .bind(ulid_to_sql_uuid(query.contractor_ulid))
+        .bind(query.contract_ulid.map(ulid_to_sql_uuid))
+        .bind(query.payslip_title)
+        .bind(query.payment_date)
+        .bind(query.begin_period)
+        .bind(query.end_period)
+        .bind(query.payslip_file)
+        .execute(&self.0)
+        .await?;
+
+        Ok(())
+    }
+}
---
+++ contractor-management-microservice/src/payslips/mod.rs
@@ -0,0 +1,139 @@
+use axum::{
+    extract::{ContentLengthLimit, Extension, Query},
+    Json,
+};
+use common_utils::{
+    custom_serde::{DateWrapper, FORM_DATA_LENGTH_LIMIT},
+    error::GlobeliseResult,
+    token::Token,
+};
+use eor_admin_microservice_sdk::AccessToken as AdminAccessToken;
+use rusty_ulid::Ulid;
+use serde::{Deserialize, Serialize};
+use serde_with::{base64::Base64, serde_as, FromInto, TryFromInto};
+use sqlx::{postgres::PgRow, FromRow, Row};
+use user_management_microservice_sdk::{AccessToken as UserAccessToken, Role};
+
+use crate::{common::ulid_from_sql_uuid, database::SharedDatabase};
+
+mod database;
+
+/// List the payslips.
+pub async fn user_payslips_index(
+    claims: Token<UserAccessToken>,
+    Query(mut query): Query<PayslipsIndexQuery>,
+    Extension(shared_database): Extension<SharedDatabase>,
+) -> GlobeliseResult<Json<Vec<PayslipsIndex>>> {
+    let database = shared_database.lock().await;
+
+    match query.role {
+        Role::Client => query.client_ulid = Some(claims.payload.ulid),
+        Role::Contractor => query.contractor_ulid = Some(claims.payload.ulid),
+    };
+
+    let result = database.payslips_index(query).await?;
+    Ok(Json(result))
+}
+
+/// List the payslips.
+pub async fn eor_admin_payslips_index(
+    _: Token<AdminAccessToken>,
+    Query(query): Query<PayslipsIndexQuery>,
+    Extension(shared_database): Extension<SharedDatabase>,
+) -> GlobeliseResult<Json<Vec<PayslipsIndex>>> {
+    let database = shared_database.lock().await;
+    let result = database.payslips_index(query).await?;
+    Ok(Json(result))
+}
+
+/// Create a payslip.
+pub async fn eor_admin_create_payslip(
+    _: Token<AdminAccessToken>,
+    ContentLengthLimit(Json(request)): ContentLengthLimit<
+        Json<CreatePayslipsIndex>,
+        FORM_DATA_LENGTH_LIMIT,
+    >,
+    Extension(shared_database): Extension<SharedDatabase>,
+) -> GlobeliseResult<()> {
+    let database = shared_database.lock().await;
+    database.create_payslip(request).await?;
+    Ok(())
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct PayslipsIndex {
+    ulid: Ulid,
+    #[serde(flatten)]
+    other_fields: PayslipsIndexSqlHelper,
+}
+
+impl<'r> FromRow<'r, PgRow> for PayslipsIndex {
+    fn from_row(row: &'r PgRow) -> Result<Self, sqlx::Error> {
+        Ok(Self {
+            ulid: ulid_from_sql_uuid(row.try_get("ulid")?),
+            other_fields: PayslipsIndexSqlHelper::from_row(row)?,
+        })
+    }
+}
+
+#[serde_as]
+#[derive(Debug, FromRow, Serialize)]
+#[serde(rename_all = "kebab-case")]
+struct PayslipsIndexSqlHelper {
+    client_name: String,
+    contractor_name: String,
+    #[serde(default)]
+    contract_name: Option<String>,
+    payslip_title: String,
+    #[serde_as(as = "FromInto<DateWrapper>")]
+    payment_date: sqlx::types::time::Date,
+    #[serde_as(as = "FromInto<DateWrapper>")]
+    begin_period: sqlx::types::time::Date,
+    #[serde_as(as = "FromInto<DateWrapper>")]
+    end_period: sqlx::types::time::Date,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct PayslipsIndexQuery {
+    #[serde(default = "PayslipsIndexQuery::default_page")]
+    pub page: i64,
+    #[serde(default = "PayslipsIndexQuery::default_per_page")]
+    pub per_page: i64,
+    pub search_text: Option<String>,
+    // NOTE: The access token should have this information instead because
+    // someone _could_ spoof if they have a similar ULID.
+    pub role: Role,
+    pub contractor_ulid: Option<Ulid>,
+    pub client_ulid: Option<Ulid>,
+}
+
+impl PayslipsIndexQuery {
+    fn default_page() -> i64 {
+        1
+    }
+
+    fn default_per_page() -> i64 {
+        25
+    }
+}
+
+#[serde_as]
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct CreatePayslipsIndex {
+    pub client_ulid: Ulid,
+    pub contractor_ulid: Ulid,
+    #[serde(default)]
+    pub contract_ulid: Option<Ulid>,
+    pub payslip_title: String,
+    #[serde_as(as = "TryFromInto<DateWrapper>")]
+    pub payment_date: sqlx::types::time::Date,
+    #[serde_as(as = "TryFromInto<DateWrapper>")]
+    pub begin_period: sqlx::types::time::Date,
+    #[serde_as(as = "TryFromInto<DateWrapper>")]
+    pub end_period: sqlx::types::time::Date,
+    #[serde_as(as = "Base64")]
+    pub payslip_file: Vec<u8>,
+}
