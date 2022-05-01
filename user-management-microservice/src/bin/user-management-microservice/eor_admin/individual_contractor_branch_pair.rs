5d7478f7cbba8331a72544c0d54691412861dc64
Hanif Bin Ariffin <hanif@globelise.com>
2022-05-01 16:13:53 +0000

    Update endpoints for individual contractor and branch pairs

--- user-management-microservice/src/bin/user-management-microservice/eor_admin/individual_contractor_branch_pair.rs
+++ user-management-microservice/src/bin/user-management-microservice/eor_admin/individual_contractor_branch_pair.rs
@@ -12,29 +12,43 @@ use crate::database::{Database, SharedDatabase};
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "kebab-case")]
-pub struct EntityContractorBranchPair {
+pub struct IndividualContractorBranchPair {
     pub contractor_ulid: Ulid,
     pub branch_ulid: Ulid,
 }
 
-impl FromRow<'_, PgRow> for EntityContractorBranchPair {
+impl FromRow<'_, PgRow> for IndividualContractorBranchPair {
     fn from_row(row: &'_ PgRow) -> Result<Self, sqlx::Error> {
-        Ok(EntityContractorBranchPair {
+        Ok(IndividualContractorBranchPair {
             contractor_ulid: ulid_from_sql_uuid(row.try_get("contractor_ulid")?),
             branch_ulid: ulid_from_sql_uuid(row.try_get("branch_ulid")?),
         })
     }
 }
 
-pub async fn eor_admin_create_entity_contractor_branch_pairs(
+pub async fn eor_admin_post_individual_contractor_branch_pairs(
     // Only for validation
     _: Token<AdminAccessToken>,
-    Json(request): Json<EntityContractorBranchPair>,
+    Json(request): Json<IndividualContractorBranchPair>,
     Extension(database): Extension<SharedDatabase>,
 ) -> GlobeliseResult<()> {
     let database = database.lock().await;
     database
-        .create_individual_contractor_branch_pairs(request.contractor_ulid, request.branch_ulid)
+        .post_individual_contractor_branch_pairs(request.contractor_ulid, request.branch_ulid)
+        .await?;
+
+    Ok(())
+}
+
+pub async fn eor_admin_delete_individual_contractor_branch_pairs(
+    // Only for validation
+    _: Token<AdminAccessToken>,
+    Json(request): Json<IndividualContractorBranchPair>,
+    Extension(database): Extension<SharedDatabase>,
+) -> GlobeliseResult<()> {
+    let database = database.lock().await;
+    database
+        .delete_individual_contractor_branch_pairs(request.contractor_ulid, request.branch_ulid)
         .await?;
 
     Ok(())
@@ -49,11 +63,11 @@ pub struct ClientContractorPairQuery {
     pub contractor_ulid: Option<Ulid>,
 }
 
-pub async fn eor_admin_client_contractor_index(
+pub async fn eor_admin_get_individual_contractor_branch_pairs(
     _: Token<AdminAccessToken>,
     Query(query): Query<ClientContractorPairQuery>,
     Extension(database): Extension<SharedDatabase>,
-) -> GlobeliseResult<Json<Vec<EntityContractorBranchPair>>> {
+) -> GlobeliseResult<Json<Vec<IndividualContractorBranchPair>>> {
     let database = database.lock().await;
     Ok(Json(
         database
@@ -64,7 +78,7 @@ pub async fn eor_admin_client_contractor_index(
 
 impl Database {
     /// Create a individual contractor and branch pair
-    pub async fn create_individual_contractor_branch_pairs(
+    pub async fn post_individual_contractor_branch_pairs(
         &self,
         contractor_ulid: Ulid,
         branch_ulid: Ulid,
@@ -83,11 +97,32 @@ impl Database {
         Ok(())
     }
 
+    /// Create a individual contractor and branch pair
+    pub async fn delete_individual_contractor_branch_pairs(
+        &self,
+        contractor_ulid: Ulid,
+        branch_ulid: Ulid,
+    ) -> GlobeliseResult<()> {
+        sqlx::query(
+            "
+            DELETE FROM
+                individual_contractor_branch_pairs 
+            WHERE
+                contractor_ulid = $1 AND
+                branch_ulid =$2",
+        )
+        .bind(ulid_to_sql_uuid(contractor_ulid))
+        .bind(ulid_to_sql_uuid(branch_ulid))
+        .execute(&self.0)
+        .await?;
+        Ok(())
+    }
+
     /// Index individual contractor and branch pairs
     pub async fn get_individual_contractor_branch_pairs(
         &self,
         query: ClientContractorPairQuery,
-    ) -> GlobeliseResult<Vec<EntityContractorBranchPair>> {
+    ) -> GlobeliseResult<Vec<IndividualContractorBranchPair>> {
         let (limit, offset) = calc_limit_and_offset(query.per_page, query.page);
 
         let result = sqlx::query_as(
